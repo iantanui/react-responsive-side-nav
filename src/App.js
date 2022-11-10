1fa05389c54777a691ea4a1762840237c0baf3d7
iantanui <rutooian@gmail.com>
2022-11-10 17:23:12 +0300

    logo name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
       <div class="logo-details">
         <i class='bx bxl-c-plus-plus icon'></i>
-        <div class="logo_name">CodingLab</div>
+        <div class="logo_name">My App</div>
         <i class='bx bx-menu' id="btn" ></i>
       </div>
 
@@ -77,8 +77,8 @@ function App() {
           <div class="profile-details">
             <img src="profile.jpg" alt="profileImg" />
             <div class="name_job">
-              <div class="name">Prem Shahi</div>
-              <div class="job">Web designer</div>
+              <div class="name">Ian Tanui</div>
+              <div class="job">Web developer</div>
             </div>
           </div>
           <i class='bx bx-log-out' id="log_out" ></i>
