1827112da2d7b2cb37e5dc3de41ae692626ab0bd
iantanui <rutooian@gmail.com>
2022-11-10 17:25:46 +0300

    input links name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
       <div class="logo-details">
         <i class='bx bxl-c-plus-plus icon'></i>
-        <div class="logo_name">My App</div>
+        <div class="logo-name">My App</div>
         <i class='bx bx-menu' id="btn" ></i>
       </div>
 
@@ -20,63 +20,63 @@ function App() {
         <li>
           <a href="#dashboard">
             <i class='bx bx-grid-alt'></i>
-            <span class="links_name">Dashboard</span>
+            <span class="links-name">Dashboard</span>
           </a>
           <span class="tooltip">Dashboard</span>
         </li>
         <li>
           <a href="#user">
             <i class='bx bx-user' ></i>
-            <span class="links_name">User</span>
+            <span class="links-name">User</span>
           </a>
           <span class="tooltip">User</span>
         </li>
         <li>
           <a href="#messages">
             <i class='bx bx-chat' ></i>
-            <span class="links_name">Messages</span>
+            <span class="links-name">Messages</span>
           </a>
           <span class="tooltip">Messages</span>
         </li>
         <li>
           <a href="#analytics">
             <i class='bx bx-pie-chart-alt-2' ></i>
-            <span class="links_name">Analytics</span>
+            <span class="links-name">Analytics</span>
           </a>
           <span class="tooltip">Analytics</span>
         </li>
         <li>
           <a href="#files">
             <i class='bx bx-folder' ></i>
-            <span class="links_name">File Manager</span>
+            <span class="links-name">File Manager</span>
           </a>
           <span class="tooltip">Files</span>
         </li>
         <li>
           <a href="#order">
             <i class='bx bx-cart-alt' ></i>
-            <span class="links_name">Order</span>
+            <span class="links-name">Order</span>
           </a>
           <span class="tooltip">Order</span>
         </li>
         <li>
           <a href="#saved">
             <i class='bx bx-heart' ></i>
-            <span class="links_name">Saved</span>
+            <span class="links-name">Saved</span>
           </a>
           <span class="tooltip">Saved</span>
         </li>
         <li>
           <a href="#setting">
             <i class='bx bx-cog' ></i>
-            <span class="links_name">Setting</span>
+            <span class="links-name">Setting</span>
           </a>
           <span class="tooltip">Setting</span>
         </li>
         <li class="profile">
           <div class="profile-details">
             <img src="profile.jpg" alt="profileImg" />
-            <div class="name_job">
+            <div class="name-job">
               <div class="name">Ian Tanui</div>
               <div class="job">Web developer</div>
             </div>
